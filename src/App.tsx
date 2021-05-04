cde29f7c8da3f168d31f536e3d02f72c30fa8037
jigsaw <m@jgs.me>
2021-05-04 19:17:57 +0900

    :hatching_chick:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
+import { parse } from '@progfay/scrapbox-parser'
 
 function App() {
+  const [src, setSrc] = useState('')
+  const onChange = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setSrc(event.target.value)
+    },
+    []
+  )
+  const parsed = useMemo(() => parse(src), [src])
   return (
-    <div>
-      <h1>It works!</h1>
+    <div className="flex h-screen">
+      <textarea
+        value={src}
+        onChange={onChange}
+        className="border border-gray-300 w-1/2 h-full"
+      ></textarea>
+      <div className="w-1/2">{JSON.stringify(parsed)}</div>
     </div>
   )
 }
