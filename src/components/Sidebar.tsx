216ba83911e28645d880e112cbef6159de0d3e21
jigsaw <m@jgs.me>
2021-09-05 10:25:14 +0900

    Create meta page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,6 @@ import {
 import { Render } from 'components/Render'
 import { useBlob } from 'hooks/useBlob'
 import Link from 'next/link'
-import format from 'date-fns/format'
 import { useRouter } from 'next/router'
 
 export const Sidebar = () => {
@@ -20,18 +19,15 @@ export const Sidebar = () => {
     }
     return ''
   }, [data])
-  const today = useMemo(() => {
-    return format(new Date(), 'yMMdd')
-  }, [])
   const [repos, setRepos] = useState([])
   useEffect(() => {
     setRepos(window.localStorage.getItem('repo').split(','))
   }, [])
   const onChange = useCallback<ChangeEventHandler<HTMLSelectElement>>(
     (event) => {
-      router.push(`/pages/${event.target.value}/${today}`)
+      router.push(`/meta/${event.target.value}/daily`)
     },
-    [router, today]
+    [router]
   )
   return (
     <div>
@@ -44,7 +40,7 @@ export const Sidebar = () => {
           ))}
         </select>
       </div>
-      <Link href={`/pages/${router.query.repo}/${today}`}>Daily Note</Link>
+      <Link href={`/meta/${router.query.repo}/daily`}>Daily Note</Link>
       <Render article={parsed}></Render>
       <Link href="/setting">setting</Link>
     </div>
